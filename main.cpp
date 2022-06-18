31e44afd04f022b1674a926b29ddf7857810d908
elrm921 <pocketrocket03@gmail.com>
2022-06-18 16:35:14 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main() {
             std::cout << "Работаю в режиме тренировки" << "\n";
         #elif MODE == 1
             std::cout << "Работаю в боевом режиме" << "\n";
-            #define ADD(a, b) ((a) + (b))
+            #include "add.h"
             int num1, num2;
             std::cout << "Введите число 1: ";
             std::cin >> num1;
             std::cout << "Введите число 2: ";
             std::cin >> num2;
-            std::cout << "Результат сложения: " << ADD(num1, num2) << "\n";
+            std::cout << "Результат сложения: " << add(num1, num2) << "\n";
         #else
             std::cout << "Неизвестный режим. Завершение работы" << "\n";
         #endif
