fae14d095779ca18eb2c565e8785e1fe1de82171
elrm921 <pocketrocket03@gmail.com>
2022-06-26 20:26:02 +0300

    move include directive and use namespace

--- add.cpp
+++ add.cpp
@@ -1,5 +1,5 @@
 #include "add.h"
 
-int add(int a, int b) {
+int functions::add(int a, int b) {
     return a + b;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
 #define MODE 1
+#if MODE == 1
+#include "add.h"
+#endif
 
 int main() {
     #ifdef MODE
@@ -8,13 +10,12 @@ int main() {
             std::cout << "Работаю в режиме тренировки" << "\n";
         #elif MODE == 1
             std::cout << "Работаю в боевом режиме" << "\n";
-            #include "add.h"
             int num1, num2;
             std::cout << "Введите число 1: ";
             std::cin >> num1;
             std::cout << "Введите число 2: ";
             std::cin >> num2;
-            std::cout << "Результат сложения: " << add(num1, num2) << "\n";
+            std::cout << "Результат сложения: " << functions::add(num1, num2) << "\n";
         #else
             std::cout << "Неизвестный режим. Завершение работы" << "\n";
         #endif
